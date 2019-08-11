3cae70caca64abbcb13f60d46944319453be76dd
Luis Paredes <lualps94@gmail.com>
2019-08-11 17:29:50 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# lualparedes.github.io
-
-This is the code for my online portfolio. This website was built using React.
---
+++ precache-manifest.0fa7cce13fa30a2b9661cd952cea6459.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab365f44bd5704e89728f7f2fc88237b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf9e0428a4f44908e90b",
+    "url": "/static/css/main.8a689157.chunk.css"
+  },
+  {
+    "revision": "b0ef65cb4dba7f0e1786",
+    "url": "/static/js/2.1dbbadc2.chunk.js"
+  },
+  {
+    "revision": "cf9e0428a4f44908e90b",
+    "url": "/static/js/main.bac12dda.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6d1b50eab93aaeeceb615a4db89bb3a",
+    "url": "/static/media/doit.b6d1b50e.png"
+  },
+  {
+    "revision": "4e9b675b966d3c45a47420c568741d5f",
+    "url": "/static/media/guia.4e9b675b.png"
+  },
+  {
+    "revision": "957f1278e312ede643eec684e22cc6db",
+    "url": "/static/media/hkgrotesk-bold-webfont.957f1278.woff2"
+  },
+  {
+    "revision": "eb46d247c50d56610c5edde0eac5ce8d",
+    "url": "/static/media/hkgrotesk-bold-webfont.eb46d247.woff"
+  },
+  {
+    "revision": "2fb50da6b95362c9b392f978360fb996",
+    "url": "/static/media/hkgrotesk-regular-webfont.2fb50da6.woff"
+  },
+  {
+    "revision": "eceb0a2f1ff2a359e67285df9ff30556",
+    "url": "/static/media/hkgrotesk-regular-webfont.eceb0a2f.woff2"
+  },
+  {
+    "revision": "3239fb09bf1c7bd7c13dd65d3ed764c4",
+    "url": "/static/media/icomoon.3239fb09.eot"
+  },
+  {
+    "revision": "987c5b74233bcdf1a72ec795f35caf46",
+    "url": "/static/media/icomoon.987c5b74.woff"
+  },
+  {
+    "revision": "d8620040077d20fa52024d16796ecf3c",
+    "url": "/static/media/icomoon.d8620040.ttf"
+  },
+  {
+    "revision": "cfc9f626c33d55f75c747c01ccc6e6cd",
+    "url": "/static/media/lual.cfc9f626.png"
+  },
+  {
+    "revision": "63db150f83c53cf519a06cdd22fb298e",
+    "url": "/static/media/oikos.63db150f.png"
+  },
+  {
+    "revision": "69b790b3f459096f3bb1a0d1be6ae217",
+    "url": "/static/media/sorter.69b790b3.png"
+  }
+]);
