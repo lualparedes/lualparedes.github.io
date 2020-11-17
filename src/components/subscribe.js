31f8e8467770a43a19a0abcd41263d09aecedc8b
Luis Paredes <lualps94@gmail.com>
2020-11-17 14:29:31 -0400

    Barebones subscribe box

---
+++ src/components/subscribe.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Image from "gatsby-image";
+
+const Subscribe = () => {
+  return (
+    <div className="subscribe">
+      <p>
+        Web dev content,
+        <br /> delivered weekly
+      </p>
+      <div id="mc_embed_signup">
+        <form
+          action="https://gmail.us2.list-manage.com/subscribe/post?u=81f5f28fdffec0498a75c1527&amp;id=cff01cbe3f"
+          method="post"
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          className="validate"
+          target="_blank"
+          novalidate
+        >
+          <div id="mc_embed_signup_scroll">
+            <div className="mc-field-group">
+              <label for="mce-NAME">Name </label>
+              <input
+                type="text"
+                defaultValue=""
+                name="NAME"
+                className="required"
+                id="mce-NAME"
+              />
+            </div>
+            <div className="mc-field-group">
+              <label for="mce-EMAIL">Email </label>
+              <input
+                type="email"
+                defaultValue=""
+                name="EMAIL"
+                className="required email"
+                id="mce-EMAIL"
+              />
+            </div>
+            <div id="mce-responses" className="clear">
+              <div
+                className="response"
+                id="mce-error-response"
+                style={{display: 'none'}}
+              ></div>
+              <div
+                className="response"
+                id="mce-success-response"
+                style={{display: 'none'}}
+              ></div>
+            </div>
+            <div style={{position: 'absolute', left: -5000, ariaHidden: 'true'}}>
+              <input
+                type="text"
+                name="b_81f5f28fdffec0498a75c1527_cff01cbe3f"
+                tabindex="-1"
+                value=""
+              />
+            </div>
+            <div className="clear">
+              <input
+                type="submit"
+                value="Subscribe"
+                name="subscribe"
+                id="mc-embedded-subscribe"
+                className="button"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Subscribe from "../components/subscribe"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
@@ -29,6 +30,7 @@ const BlogPostTemplate = ({ data, location }) => {
           dangerouslySetInnerHTML={{ __html: post.html }}
           itemProp="articleBody"
         />
+        <Subscribe />
         <hr />
         <footer>
           <Bio />
